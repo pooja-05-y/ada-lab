f08f592c9c4435b3b6ad7a4fd6e9a9975be55ac1
Pooja Y <pooja.y011235@gmail.com>
2026-04-22 14:51:57 +0530

    week-6 floyds

---
+++ fl1.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+#define MAX 100
+#define INF 99999
+void floydWarshall(int n, int graph[MAX][MAX]) {
+    int dist[MAX][MAX];
+
+    // Copy graph to dist
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            dist[i][j] = graph[i][j];
+        }
+    }
+
+    // Floyd Warshall
+    for (int k = 0; k < n; k++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (dist[i][k] + dist[k][j] < dist[i][j]) {
+                    dist[i][j] = dist[i][k] + dist[k][j];
+                }
+            }
+        }
+    }
+
+    // Output result
+    printf("\nShortest Distance Matrix:\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (dist[i][j] == INF)
+                printf("INF ");
+            else
+                printf("%d ", dist[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int n;
+    int graph[MAX][MAX];
+
+    printf("Enter number of vertices: ");
+    scanf("%d", &n);
+
+    printf("Enter adjacency matrix (use %d for INF):\n", INF);
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &graph[i][j]);
+        }
+    }
+
+    floydWarshall(n, graph);
+
+    return 0;
+}
